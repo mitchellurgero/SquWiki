fa6a0c5b34dec38118f4e1539380cbc111a56de7
MitchellUrgero <mitchellurgero@gmail.com>
2015-07-17 09:48:54 -0500

    Added a ton of comments to the index.php file for easy editing later.

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 require_once('config.php');
 require_once('parsedown.php');
 require_once('parsedownextra.php');
-
+//Begin Parsing request URI to get what file the user needs
 $request  = str_replace($pageFolder, "", $_SERVER['REQUEST_URI']);
 // $params = end(explode("/"), $request);
 $params = split("/", $request);
+//List any safe pages the user is allow to view (Any file in pages folder)
 $safe_pages = scandir("./pages");
+//Process request
   if(in_array($params[1].".md", $safe_pages)) {
     //process page
 	echo "<html>";
 	echo "\n";
     readPage($params[1]);
-    
     echo "</html>";
   } elseif($params[1] === "") {
     readPage("home");
@@ -25,14 +26,22 @@ $safe_pages = scandir("./pages");
 
 
 //functions
+
+/*
+The readPage function does exactly as it says on the tin: Read the <page>.md file and output the result to the page
+This uses Parsedown & ParsedownExtra to read markdown quickly, then output.
+*/
 function readPage($page){
 	$pd = new ParsedownExtra();
 	$page_id = "./pages/".$page.".md";
+	//Read file into an array for easy access.
 	$lines = file($page_id, FILE_IGNORE_NEW_LINES);
+	//Begin output of header
 	echo "<head>";
 	echo "\n";
 	echo'<header>'."\n";
-	global $SiteName, $Description;
+	//Get variables from config.php to determin Site name, Description, and if admin is allowing public edit access.
+	global $SiteName, $Description, $AllowPublicEdit;
 	echo "<h3>".$SiteName."</h3>"."\n";
 	echo $Description."<br />"."\n";
 	echo'</header>'."\n";
@@ -40,6 +49,7 @@ function readPage($page){
 	echo '<div id="content">';
     echo '<div id="mainContent">';
 	echo '<section id="intro">';
+	//Parse the first 4 lines of <page>.md file for Page Title, description, and keywords (SEO meta data for better Google results.)
 	foreach($lines as $line_num => $line)
 	{
 		// $line = str_replace("\n","", $line);
@@ -63,17 +73,26 @@ function readPage($page){
 	echo "</section>";
 	css();
 	echo "\n";
+	//Output any javascript that may or may not be needed. (IF the file is blank, then nothing will be there.) **Jquery is already added from a CDN for ease of use as well.
     javascript();
 	echo "</head>";
 	echo "\n";
 	echo '<body>';
-	echo "<section>";
+	echo '<section id="bodyContents">';
 	$pageString = file_get_contents($page_id);
+	//Remove first four lines of the page array (Not the page file) so we do not output it to the page in HTML.
 	$pageString = implode("\n", array_slice(explode("\n", $pageString), 4));
+	//Push page through the ParsedownExtra class
 	echo $pd->text($pageString);
 	$filename = $page_id;
 	if (file_exists($filename)) {
     	echo '<p style="font-size: 12px;"><i>This page was last modified: '.date ("F d Y H:i:s.", filemtime($filename)).'</i></p>';
+    	if($filename != "./pages/404.md"){
+    		//If page is not the 404 page, then detect if Public edits is on.
+    		if($AllowPublicEdit === true){
+    			echo'<p><a href="#">edit this page</a></p>';
+    		}
+    	}
 	}
 	echo "</section>";
 	
@@ -84,12 +103,17 @@ function readPage($page){
 	echo "</body>";
 	echo "\n";
 }
+/*
+Simple Menu function to get a basic menu, file is in: ./application/menu.md 
+Use this file to build the top menu.
+*/
 function menu(){
 	$pd = new ParsedownExtra();
 	$page_id = "./application/menu.md";
 	$lines = file($page_id);
 	echo "<nav>"."\n";
 	echo "<ul>"."\n";
+	//We put the menu in a for loop to separate the links into the nav properly.
 	foreach($lines as $line_num => $line){
 		$line2 = $pd->text($line);
 		echo "<li>".$line2."</li>"."\n";
@@ -97,6 +121,10 @@ function menu(){
 	echo "</ul>"."\n";
 	echo "</nav>"."\n";
 }
+/*
+The aside function is used to build a sidebar of sorts, very basic, but supports all your HTML and javascript easily. This file is: ./application/aside.md
+This file is not put through a for loop so that everything in the file is put right next to each other (Not in looks, but in code.)
+*/
 function aside(){
 	$pd = new ParsedownExtra();
 	$page_id = "./application/aside.md";
@@ -104,7 +132,7 @@ function aside(){
 	echo $pd->text(file_get_contents($page_id));
 	echo "</aside>";
 }
-//CSS
+//CSS - Self-explanitory, Parses the ./css/style.css into the html (As opposeed to a seprate file) This function is only temporary because of an htacceess bug I am working on.
 function css(){
 	echo "<style>";
 	echo "\n";
@@ -113,7 +141,7 @@ function css(){
 	echo "\n";
 }
 
-//Javascript
+//Javascript - Parses the ./js/index.js file into the HTML.
 function javascript(){
 	echo '';
 	?>
